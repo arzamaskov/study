aca7b1aa3b4ba2a3b0a15560777df119c2308d1f
Andrey Arzamaskov <aarzamaskov@simtechdev.org>
2021-12-14 15:14:41 +0400

    fix: add solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,32 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        result = []
+        array.each do |item|
+          result << (item.positive? ? array.max : item)
+        end
+        result
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        left = -1
+        right = array.size
+
+        while right - left > 1
+          middle = left + ((right - left) / 2)
+
+          if array[middle] >= query
+            right = middle
+          else
+            left = middle
+          end
+        end
+
+        if array[right] == query
+          right
+        else
+          -1
+        end
       end
     end
   end
