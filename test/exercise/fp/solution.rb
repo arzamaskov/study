ef37405c53c0bebc5ae0d0f8ef4a9194a4241f86
Andrey Arzamaskov <aarzamaskov@simtechdev.org>
2021-12-24 11:40:52 +0400

    fix: add solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,26 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+
+      def get_films_by_rating(array)
+        array.select { |film| film if film['rating_kinopoisk'].present? && !film['rating_kinopoisk'].to_f.zero? }
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def get_films_by_country(array)
+        array.select { |film| film if film['country'].present? && film['country'].split(',').size > 1 }
+      end
+
+      def rating(array)
+        films = get_films_by_rating(array)
+        films = get_films_by_country(films)
+        rating_kinopoisk = films.map { |film| film['rating_kinopoisk'].to_f }
+        rating_kinopoisk.reduce(:+) / rating_kinopoisk.size
+      end
+
+      def chars_count(films, threshold)
+        film_names = films.select { |film| film if film['rating_kinopoisk'].to_f >= threshold }
+
+        film_names.map { |film| film['name'].count('и') }.reduce(:+)
       end
     end
   end
