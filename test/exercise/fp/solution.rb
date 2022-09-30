20e2d101d926b1ac83e9de0e23ec3324b7ba1a12
Andrey Arzamaskov <aarzamaskov@simtechdev.org>
2022-09-30 17:26:35 +0400

    rework solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,25 +4,23 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-
-      def get_films_by_rating(array)
-        array.select { |film| film if film['rating_kinopoisk'].present? && !film['rating_kinopoisk'].to_f.zero? }
-      end
-
-      def get_films_by_country(array)
-        array.select { |film| film if film['country'].present? && film['country'].split(',').size > 1 }
+      def validate(film)
+        film if film['country'] && film['country'].split(',').size > 1 && !film['rating_kinopoisk'].to_f.zero?
       end
 
       def rating(array)
-        films = get_films_by_country(get_films_by_rating(array))
-        rating_kinopoisk = films.map { |film| film['rating_kinopoisk'].to_f }
-        rating_kinopoisk.reduce(:+) / rating_kinopoisk.size
+        validated_films = array.map { |film| validate(film) }.compact
+        ratings_kinopoisk = validated_films.map { |film| film['rating_kinopoisk'].to_f }.compact
+        ratings_kinopoisk.reduce(:+) / ratings_kinopoisk.size
+      end
+
+      def get_name(film, threshold)
+        film['name'] if film['rating_kinopoisk'].to_f > threshold
       end
 
       def chars_count(films, threshold)
-        film_names = films.select { |film| film if film['rating_kinopoisk'].to_f >= threshold }
-
-        film_names.map { |film| film['name'].count('и') }.reduce(:+)
+        names = films.map { |film| get_name(film, threshold) }.compact
+        names.map { |name| name.count('и') }.reduce(:+)
       end
     end
   end
